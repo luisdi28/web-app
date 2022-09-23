10210eeba5fba814513d385a20ef5fcc2ae15d23
Luis <luisdi.28@estudiantec.cr>
2022-09-22 20:06:21 -0600

    Actividad 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { fetchHelloWorld } from './services/HelloWorldService';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [label, setLabel] = useState("");
+const clickHandler = async () => {
+const result = await fetchHelloWorld();
+setLabel(result);
+};
+return (
+<div className="App">
+<button onClick={() => clickHandler()}>Call API</button>
+{label}
+</div>
+);
 }
-
 export default App;
